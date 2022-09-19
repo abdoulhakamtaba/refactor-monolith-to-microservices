fb0216945c06331d7707251f2bfe17ab09721f2a
abdoulhakamtaba <abdoulhakamtaba@gmail.com>
2022-09-19 15:51:01 +0100

    Made some other few changes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8a75163b082f486f90b70d07f3c7fc0-1006431359.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
